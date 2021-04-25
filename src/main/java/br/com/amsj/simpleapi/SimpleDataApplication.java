808fba901aba6cfc43e38dfbb5c010c9ed1c44c9
ailton.msj <ailton.msj@gmail.com>
2021-04-25 12:22:51 -0300

    first commit

--- src/main/java/br/com/amsj/simpleapi/SimpleDataApplication.java
+++ src/main/java/br/com/amsj/simpleapi/SimpleDataApplication.java
@@ -2,10 +2,12 @@ package br.com.amsj.simpleapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class SimpleDataApplication {
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SimpleDataApplication.class, args);
 	}
--- src/main/java/br/com/amsj/simpleapi/controller/DateController.java
+++ src/main/java/br/com/amsj/simpleapi/controller/DateController.java
@@ -4,12 +4,16 @@ import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.Date;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DateController {
+	
+	private final Logger log = LoggerFactory.getLogger(this.getClass());
 	
 	@RequestMapping(method=RequestMethod.GET, path="/")
 	public String getCurrentDate() {
@@ -19,8 +23,13 @@ public class DateController {
 			server = InetAddress.getLocalHost().getCanonicalHostName();
 		} catch (UnknownHostException e) {
 			e.printStackTrace();
+			log.error(e.getMessage());
 		}
 		
-		return server + " - " + new Date();
+		String data = server + " - " + new Date() + " - " + " from redundant";
+		
+		log.info("Success on Redundant");
+		
+		return data;
 	}
 }
